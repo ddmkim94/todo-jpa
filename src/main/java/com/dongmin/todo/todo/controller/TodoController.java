605f291d55af69b11968aae2e27ecea0954bc716
ddmkim94 <ddmkim94@gmail.com>
2022-12-15 16:58:23 +0900

    #7 메인화면 작업 끝

--- src/main/java/com/dongmin/todo/todo/controller/TodoController.java
+++ src/main/java/com/dongmin/todo/todo/controller/TodoController.java
@@ -3,16 +3,13 @@ package com.dongmin.todo.todo.controller;
 import com.dongmin.todo.todo.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/todo")
 public class TodoController {
 
     private final TodoService todoService;
 
-    @GetMapping("/")
-    public String list() {
-        return "todo/todo-list";
-    }
 }
